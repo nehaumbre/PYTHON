40b70600a07de129438067d2aea048f840af64c6
nehaumbre <nehaumbre28@gmail.com>
2026-04-07 16:44:17 +0530

    00:00 - Intro 2:29 - What you'll learn 4:18 - Course Setup 12:13 - Variables In Depth 17:29 - Identifiers In Depth 26:05 - Arithmetic Operation 32:54 - Getting Inputs From The User 35:54 - Assignment Operators 39:53 - Strings In Depth

---
+++ index.py
@@ -0,0 +1,94 @@
+print("Hello World")
+# this is a comment
+# variables
+# age =30
+# print(age)
+# age = 10
+# print(age)
+
+# arithmetic operations
+# sum_result = 6+6
+# print("SUM:",sum_result)
+# difference_result = 6-6
+# print("DIFFERENCE:",difference_result)
+# product_result = 6*6
+# print("PRODUCT:",product_result)
+# quotient_result = 6/6
+# print("QUOTIENT:",quotient_result)
+# power_result = 6**6
+# print("POWER:",power_result)
+# modulus_result = 6%6
+# print("MODULUS:",modulus_result)
+
+# rounded_result = 30//2
+# print("ROUNDED:",rounded_result)
+
+
+# Inputs
+# name = input("What is your name? ")
+# print("Hello", name)
+
+#assignment operators
+# x= 5
+# x+=3 # x= x+3
+# print(x)
+# x-=3 # x= x-3
+# print(x)
+# x*=3 # x= x*3
+# print(x)
+# x/=3 # x= x/3
+# print(x)
+# x%=3 # x= x%3
+# print(x)
+# x**=3 # x= x**3
+# print(x)
+# x//=3 # x= x//3
+# print(x)
+
+# strings
+
+# single_line_string = "This is a single line string"
+# multi_line_string = """This is a multi 
+# line string that can span
+# multiple lines without the need for escape characters."""
+# print(single_line_string)
+# print()
+# print(multi_line_string)
+
+# string concatenation
+# first_name = "John"
+# last_name = "Doe"
+# full_name = first_name + " " + last_name
+# print("Full Name:", full_name)
+# print(len(full_name))
+
+
+# String indexing and slicing
+# text = "Hello, World!"
+# first_character = text[0]
+# print("First Character:", first_character)
+# last_character = text[-1]
+# print("Last Character:", last_character)
+# substring = text[7:10]
+# print("Substring:", substring)
+
+# String formatting
+# name = "Alice"
+# age = 25
+# message = f"My name is {name} and I am {age} years old."
+# print(message)
+# formatted_message = f"2+2={2+2}"
+# print(formatted_message)
+
+
+# Escape characters
+# text_with_newline = "Hello,\nWorld!"
+# print(text_with_newline)
+# text_with_tab = "Hello,\tWorld!"
+# print(text_with_tab)
+# text_with_quotes = "She said, \"Hello!\" "
+# print(text_with_quotes)
+# text_with_backslash = "C:\\path\\to\\file"
+# print(text_with_backslash)
+
+#booleans
